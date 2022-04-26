c7d3fa665fd65c0e6bf4309a84d9280fbb028f5e
phgag <ph.gagneux@gmail.com>
2022-04-26 18:19:55 +0200

    Method test

--- src/main/java/process/LostOnes/LostOnesApplication.java
+++ src/main/java/process/LostOnes/LostOnesApplication.java
@@ -10,7 +10,7 @@ public class LostOnesApplication {
     public static void main(String[] args) {
 
 
-
+// MySQL Connection: PG
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root", "LostOnesPW");
 
@@ -21,13 +21,21 @@ public class LostOnesApplication {
             while (resultSet.next()) {
 
                 System.out.println(resultSet.getString("id"));
+                System.out.println(resultSet.getString("cost"));
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+// Method: id extractor from input values   - id is defined as. 1 = 1 pal bis 30km, 2 = 2 pal bis 30km..13 =1 pal bis 60km etc. PG
+public static void idExtract(int pal, double distance) {
+            double d;
+            d = distance;
 
+            distance / 30 = remainder;
+            distance -=
 
+        }
     }
 }
