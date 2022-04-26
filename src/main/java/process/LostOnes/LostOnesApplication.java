fe69009c96adea4f7a896a7c90d6f369d09c84d4
phgag <ph.gagneux@gmail.com>
2022-04-26 13:23:14 +0200

    test2

--- src/main/java/process/LostOnes/LostOnesApplication.java
+++ src/main/java/process/LostOnes/LostOnesApplication.java
@@ -1,8 +1,33 @@
 package process.LostOnes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class LostOnesApplication {
 
     public static void main(String[] args) {
-        System.out.println("test3");
+
+
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhohst:3306/jdbc", "root", "xxx");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from transportcosts");
+
+            while (resultSet.next()) {
+
+                System.out.println(resultSet.getString("id"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
